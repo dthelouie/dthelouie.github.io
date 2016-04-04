6d250cddac16306526b43c5ff5e647d29354beac
David Louie <dlouie1492@gmail.com>
2016-04-04 16:33:06 -0400

    Add swipe listeners with jQuery mobile

--- projects/2048-clone/js/application.js
+++ projects/2048-clone/js/application.js
@@ -1,4 +1,70 @@
 $(document).ready(function() {
+	var supportTouch = $.support.touch,
+            scrollEvent = "touchmove scroll",
+            touchStartEvent = supportTouch ? "touchstart" : "mousedown",
+            touchStopEvent = supportTouch ? "touchend" : "mouseup",
+            touchMoveEvent = supportTouch ? "touchmove" : "mousemove";
+    $.event.special.swipeupdown = {
+        setup: function() {
+            var thisObject = this;
+            var $this = $(thisObject);
+            $this.bind(touchStartEvent, function(event) {
+                var data = event.originalEvent.touches ?
+                        event.originalEvent.touches[ 0 ] :
+                        event,
+                        start = {
+                            time: (new Date).getTime(),
+                            coords: [ data.pageX, data.pageY ],
+                            origin: $(event.target)
+                        },
+                        stop;
+
+                function moveHandler(event) {
+                    if (!start) {
+                        return;
+                    }
+                    var data = event.originalEvent.touches ?
+                            event.originalEvent.touches[ 0 ] :
+                            event;
+                    stop = {
+                        time: (new Date).getTime(),
+                        coords: [ data.pageX, data.pageY ]
+                    };
+
+                    // prevent scrolling
+                    if (Math.abs(start.coords[1] - stop.coords[1]) > 10) {
+                        event.preventDefault();
+                    }
+                }
+                $this
+                        .bind(touchMoveEvent, moveHandler)
+                        .one(touchStopEvent, function(event) {
+                    $this.unbind(touchMoveEvent, moveHandler);
+                    if (start && stop) {
+                        if (stop.time - start.time < 1000 &&
+                                Math.abs(start.coords[1] - stop.coords[1]) > 30 &&
+                                Math.abs(start.coords[0] - stop.coords[0]) < 75) {
+                            start.origin
+                                    .trigger("swipeupdown")
+                                    .trigger(start.coords[1] > stop.coords[1] ? "swipeup" : "swipedown");
+                        }
+                    }
+                    start = stop = undefined;
+                });
+            });
+        }
+    };
+    $.each({
+        swipedown: "swipeupdown",
+        swipeup: "swipeupdown"
+    }, function(event, sourceEvent){
+        $.event.special[event] = {
+            setup: function(){
+                $(this).bind(sourceEvent, $.noop);
+            }
+        };
+    });
+    
 	game = new Game("0000000000000000")
 
 	updateGrid = function(game){
@@ -27,15 +93,38 @@ $(document).ready(function() {
 		e.preventDefault();
 		if (e.which == 37) {
 			game.left()
+			updateGrid(game);
 		} else if (e.which == 38) {
 			game.up()
+			updateGrid(game);
 		} else if (e.which == 39) {
 			game.right()
+			updateGrid(game);
 		} else if (e.which == 40) {
 			game.down()
+			updateGrid(game);
 		} else if (e.which == 82){
 			game = new Game("0000000000000000")
+			updateGrid(game);
 		}
-		updateGrid(game);
 	})
+
+
+	$(document).on("swipeleft", function(){
+		game.left()
+		updateGrid(game)
+	})
+	$(document).on("swiperight", function(){
+		game.right()
+		updateGrid(game)
+	})
+	$(document).on("swipeup", function(){
+		game.up()
+		updateGrid(game)
+	})
+	$(document).on("swipedown", function(){
+		game.down()
+		updateGrid(game)
+	})
+
 });
